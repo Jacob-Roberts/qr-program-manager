06de6cab35d59a5dabb67d1e66f7f34dc3c472dc
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-06-21 18:54:37 +0100

    Cutest ignore e2e

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   css: { postcss: { plugins: [] } },
   test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'
+    ]
     setupFiles: ["./tests/setup/setup-test-env.ts"],
     environment: "jsdom",
   },
