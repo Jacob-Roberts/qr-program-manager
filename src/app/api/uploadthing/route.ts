43621381444adc2731e1ff8dce62745121143fae
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-10-30 17:25:15 +0800

    Move to upload thing token

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,9 +1,12 @@
 import { createRouteHandler } from "uploadthing/next";
 
+import { env } from "#/env";
 import { ourFileRouter } from "./core";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {},
+  config: {
+    token: env.UPLOADTHING_TOKEN,
+  },
 });
--- src/env.js
+++ src/env.js
@@ -21,6 +21,7 @@ export const env = createEnv({
     // APPLE_CLIENT_SECRET: z.string(),
     UPLOADTHING_APP_ID: z.string(),
     UPLOADTHING_SECRET: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
     RESEND_API_KEY: z.string(),
     ENABLE_EMAIL_INVITES: z.coerce.boolean().optional().default(false),
     OVERRIDE_ENABLE_SHARE_WITH_FRIENDS: z.coerce
@@ -59,6 +60,7 @@ export const env = createEnv({
       process.env.OVERRIDE_ENABLE_SHARE_WITH_FRIENDS,
     UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXT_PUBLIC_PLAUSIBLE_DOMAIN: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN,
     NEXT_PUBLIC_PLAUSIBLE_CUSTOM_DOMAIN:
