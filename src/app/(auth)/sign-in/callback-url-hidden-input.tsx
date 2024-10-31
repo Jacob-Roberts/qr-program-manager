c97ea30c848c91a99a064caceefaf5e90c5b07a7
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-10-31 19:23:45 +0800

    Stop using nextjs useSearchParams

--- src/app/(auth)/sign-in/callback-url-hidden-input.tsx
+++ src/app/(auth)/sign-in/callback-url-hidden-input.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+import { useEffect, useState } from "react";
 
+// This could be loaded on the server, but that would require making this route by dynamically loaded.
+// I want it to be statically loaded, so I'm okay with this callback functionality to be missing if the
+// browser hasn't yet loaded JS and they submit.
 export function CallbackUrlHiddenInput() {
-  return (
-    <Suspense>
-      <Internal />
-    </Suspense>
-  );
-}
+  const [callbackUrl, setCallbackUrl] = useState("");
+  useEffect(() => {
+    // Check if window is defined (i.e., we're in the browser)
+    if (typeof window !== "undefined") {
+      const params = new URLSearchParams(window.location.search);
+      const url = params.get("callbackUrl");
 
-function Internal() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callback_url");
-  return <input type="hidden" name="callbackUrl" value={callbackUrl ?? ""} />;
+      if (url) {
+        setCallbackUrl(url);
+      }
+    }
+  }, []);
+  return <input type="hidden" name="callbackUrl" value={callbackUrl} />;
 }
